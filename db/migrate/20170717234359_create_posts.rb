c7b8931fa53e3cd241c33ca89c0596c904f28ef1
Felipe <felipezb@live.com>
2017-07-19 04:58:07 -0300

    Devise
    Users login

---
+++ db/migrate/20170717234359_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.text :address
+      t.float :longitude
+      t.float :latitude
+
+      t.timestamps
+    end
+  end
+end
